c923ab6c5ef77c579dc4dc81f4271f909185cd5f
Dmytro <beerandhot@gmail.com>
2023-12-10 15:16:30 +0200

    set LICENSE to MIT

--- open-color.el
+++ open-color.el
@@ -1,25 +1,14 @@
 ;;; open-color.el --- Open Color color palette       -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2022 DK
-
-;; Author: DK <a13@github.com>,
+;; Author: DK <a13@users.noreply.github.com>,
 ;; Homepage: https://github.com/a13/open-color.el
 ;; Package-Requires: ((emacs "25.1"))
 ;; Keywords: faces
 ;; Version: 0.0.1
 
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
+;;; License:
 
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;; MIT License, see accompanying LICENSE file: https://github.com/a13/open-color.el/blob/master/LICENSE
 
 ;;; Commentary:
 
