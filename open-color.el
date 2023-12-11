3bb9837a6a7a7efcbae209f9e853f2a66cf6760c
Dmytro <beerandhot@gmail.com>
2023-12-11 04:54:18 +0200

    limit line length

--- open-color.el
+++ open-color.el
@@ -12,7 +12,8 @@
 
 ;;; Commentary:
 
-;; Open color is an open-source color scheme optimized for UI like font, background, border, etc.  Based on https://github.com/yeun/open-color.
+;; Open color is an open-source color scheme optimized for UI like font, background, border, etc.
+;; Based on https://github.com/yeun/open-color.
 
 ;;; Code:
 
